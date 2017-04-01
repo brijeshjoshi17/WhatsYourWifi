b368b07e774f98554af4d5c6993d4d9ed6d7f570
Brijesh Joshi <brijesh17@gmail.com>
2017-04-01 13:11:23 -0400

    Get and show current wifi ssid

--- app/src/main/java/brijeshjoshi/whatsyourwifi/MainActivity.java
+++ app/src/main/java/brijeshjoshi/whatsyourwifi/MainActivity.java
@@ -1,7 +1,14 @@
 package brijeshjoshi.whatsyourwifi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView wifiInfoTextView = (TextView) findViewById(R.id.your_wifi_textView);
+        wifiInfoTextView.setText(getCurrentSsid(this));
     }
+
+    private static String getCurrentSsid(Context context) {
+        String ssid = null;
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context
+                .CONNECTIVITY_SERVICE);
+
+        if (connManager != null) {
+            Network[] networks = connManager.getAllNetworks();
+            for (Network network : networks) {
+                NetworkInfo networkInfo = connManager.getNetworkInfo(network);
+                if (networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI
+                        && networkInfo.isConnected()) {
+                    final WifiManager wifiManager = (WifiManager) context.getApplicationContext()
+                            .getSystemService(Context.WIFI_SERVICE);
+                    final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+                    if (connectionInfo != null) {
+                        ssid = connectionInfo.getSSID();
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ssid;
+    }
+
 }
